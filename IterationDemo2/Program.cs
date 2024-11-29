17038da4284707e25c646cf7fc02cf84f4a47b0b
Renuka Ajay Chougule <renukachougule.0523@gmail.com>
2024-11-29 23:13:31 +0530

    Add one more changes in this

--- IterationDemo2/Program.cs
+++ IterationDemo2/Program.cs
@@ -14,12 +14,64 @@
             //{
             //    Console.WriteLine(character);
             //}
-           
-            var numbers = new int [] { 1, 2, 3,4};
-            foreach (int number in numbers) 
+
+            //var numbers = new int [] { 1, 2, 3,4};
+            //foreach (int number in numbers) 
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //var i = 0;
+
+            //while (i < 10) 
+            //{
+            //    if (i % 10 == 0) 
+            //    {
+            //        Console.WriteLine(i);
+
+            //        i++;
+            //    }
+
+            //}
+
+            //while (true)
+            //{
+            //    Console.Write("Type Your Name:");
+
+            //    var input = Console.ReadLine();
+
+            //    if (string.IsNullOrEmpty(input))
+            //        break;
+            //    {
+            //        Console.WriteLine("@Echo:" + input);
+                  
+
+            //    }
+
+               
+
+
+            //}
+
+            while (true)
             {
-                Console.WriteLine(number);
+                Console.Write("Type Your Name:");
+
+               var input = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(input))
+
+                {
+                    Console.WriteLine("@Echo:" + input);
+                    break;
+
+                }
+
+                continue;
+
+
             }
         }
+
     }
 }
